5abd2f4e3d73da052de438a567c4b4c20805458b
Cesar Morales <cmorales@thedataage.com>
2022-02-24 22:54:02 -0600

    Add project files.

---
+++ BlazorAppChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorAppChat.Server.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ BlazorAppChat/Server/Program.cs
@@ -0,0 +1,44 @@
+using BlazorAppChat.Server.Hubs; // Added
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR(); // Added
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults
+    .MimeTypes.Concat(new[] { "application/octet-stream" });
+}); // Added
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapHub<ChatHub>("/chatHub"); // Added
+app.MapFallbackToFile("index.html");
+
+app.Run();
